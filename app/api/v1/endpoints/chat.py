1537c3041860b00ad251f0decf974d52f6133458
manojKanisol <mp.varma@kanisol.com>
2025-02-17 20:57:38 +0530

    add chat history api

--- app/api/v1/endpoints/chat.py
+++ app/api/v1/endpoints/chat.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException,Depends,Request,Header
 from pymongo import MongoClient
 from langchain_mongodb import MongoDBAtlasVectorSearch
+from langchain_mongodb.chat_message_histories import MongoDBChatMessageHistory
 from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -35,10 +36,13 @@ client = MongoClient(settings.mongodb_url)
 
 #MongoDB Database Collection
 DB_NAME = "vector"
+CHAT_DB_NAME = "chat"
+CHAT_COLLECTION_NAME = "chat_histories"
 COLLECTION_NAME = "vector_collection"
 ATLAS_VECTOR_SEARCH_INDEX_NAME = "rag-index-vectors"
 
 MONGODB_COLLECTION = client[DB_NAME][COLLECTION_NAME]
+MONGODB_COLLECTION_HISTORY = client
 
 #VectorMongdbSearch
 mongodb_vector_search = MongoDBAtlasVectorSearch(
@@ -67,7 +71,6 @@ Answer the following question:
 # Chat Streaming
 @router.post("/chat_streaming")
 async def chat_streaming(data:UserPrompt,userId:str = Depends(get_token_from_header),Authorization:Optional[str] = Header(None)):
-    print(data.prompt)
     docs = mongodb_vector_search.similarity_search(query=data.prompt,k=2)
     print(docs)
     rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
@@ -83,9 +86,31 @@ async def chat_streaming(data:UserPrompt,userId:str = Depends(get_token_from_hea
     )
 
     result = chain.invoke({"context":docs,"question":data.prompt})
+    chat_message_history = MongoDBChatMessageHistory(
+        session_id= userId,
+        connection_string=settings.mongodb_url,
+        database_name=CHAT_DB_NAME,
+        collection_name=CHAT_COLLECTION_NAME
+    )
+    if result:
+        chat_message_history.add_user_message(data.prompt)
+        chat_message_history.add_ai_message(result)
 
     return {
         "generated_content": result
     }
+
+@router.post("/chat_history")
+async def chat_history(userId:str = Depends(get_token_from_header),Authorization:Optional[str] = Header(None)):
+     chat_message_history = MongoDBChatMessageHistory(
+        session_id= userId,
+        connection_string=settings.mongodb_url,
+        database_name=CHAT_DB_NAME,
+        collection_name=CHAT_COLLECTION_NAME
+    )
+     return{
+         "chat_history":chat_message_history.messages
+     }
+
 
 # uvicorn.run(app)
